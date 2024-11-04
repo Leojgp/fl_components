ee153e38ab953ba442dba72d41ff71791503ee99
Leojgp <leonardoljgp@gmail.com>
2024-11-04 12:25:28 +0100

    Mi primer Commit

---
+++ lib/screens/listview1_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Listview1Screen extends StatelessWidget {
+  final options = const[''];
+  const Listview1Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Listview Tipo 1'),
+        ),
+        body: ListView(
+          children: const [
+            ListTile(
+              leading: Icon(Icons.access_time_sharp),
+              title: Text('Hola Mundo'),
+            )
+          ],
+        )
+        );
+  }
+}
