c1cbbc961a75fcec008e67d943301dcc0aec6d7b
Leojgp <leonardoljgp@gmail.com>
2024-11-08 08:43:11 +0100

    Clase screens creadas que exporta todas las screens para limpieza de código

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'package:fl_components/screens/alert_screen.dart';
-import 'package:fl_components/screens/card_screen.dart';
-import 'package:fl_components/screens/home_screen.dart';
-import 'package:fl_components/screens/listview1_screen.dart';
-import 'package:fl_components/screens/listview2_screen.dart';
 import 'package:flutter/material.dart';
+import 'screens/screens.dart';
 
 void main() => runApp(const MyApp());
 
@@ -17,11 +13,11 @@ class MyApp extends StatelessWidget {
       title: 'Material App',
       home: const Listview2Screen(),
       routes: {
-        'listview1': (BuildContext context) => Listview1Screen(),
-        'listview2': (BuildContext context) => Listview2Screen(),
-        'alert': (BuildContext context) => AlertScreen(),
-        'card': (BuildContext context) => CardScreen(),
-        'home': (BuildContext context) => HomeScreen(),
+        'listview1': (BuildContext context) => const Listview1Screen(),
+        'listview2': (BuildContext context) => const Listview2Screen(),
+        'alert': (BuildContext context) => const AlertScreen(),
+        'card': (BuildContext context) => const CardScreen(),
+        'home': (BuildContext context) => const HomeScreen(),
       },
     );
   }
