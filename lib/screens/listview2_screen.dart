2a38f8c14265e320910cddaa74d71539a58feccc
LeonardoLJGP <lgarpau0908@ieszaidinvergeles.org>
2024-11-22 16:37:02 +0100

    Corrección en la creación de las screens de cada casa

--- lib/screens/listview2_screen.dart
+++ lib/screens/listview2_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Listview2Screen extends StatelessWidget {
-  final options = const['Targaryen','Stark','Lannister','Baratheon'];
+  final options = const ['Targaryen', 'Stark', 'Lannister', 'Baratheon'];
   const Listview2Screen({Key? key}) : super(key: key);
 
   @override
@@ -12,17 +12,41 @@ class Listview2Screen extends StatelessWidget {
           title: const Text('Listview Tipo 2'),
         ),
         body: ListView.separated(
-          itemBuilder: (context, index) => ListTile(
-              trailing: Icon(Icons.arrow_forward_ios_outlined),
-              title: Text(options[index]),
-              onTap: (){
-                final casa = options[index];
-                print('casa');
-              } ),
-          // Cada vez que itere en options pondrá un divider
-          separatorBuilder: (context, index) => const Divider(), 
-          itemCount: options.length
-          )
-        );
+            itemBuilder: (context, index) => ListTile(
+                trailing: Icon(Icons.arrow_forward_ios_outlined),
+                title: Text(options[index]),
+                onTap: () {
+                  final casa = options[index];
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CasasScreen(casa: casa),
+                    ),
+                  );
+                }),
+            // Cada vez que itere en options pondrá un divider
+            separatorBuilder: (context, index) => const Divider(),
+            itemCount: options.length));
+  }
+}
+
+class CasasScreen extends StatelessWidget {
+  final String casa;
+
+  const CasasScreen({Key? key, required this.casa}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Casa $casa'),
+      ),
+      body: Center(
+        child: Text(
+          'Bienvenido a la casa $casa',
+          style: const TextStyle(fontSize: 24),
+        ),
+      ),
+    );
   }
 }
