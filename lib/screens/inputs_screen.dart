c5009bb2ba13698a4734c4e091c336e0d052a870
Leojgp <leonardoljgp@gmail.com>
2024-11-29 09:07:12 +0100

    Personalizando el InputsScreen

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -11,7 +11,42 @@ class InputsScreen extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Text('InputsScreen'),
+        child: Column(
+          children: [
+            TextFormField(
+              autofocus: true,
+              //initialValue: 'Introduce tu nombre',
+              textCapitalization: TextCapitalization.words,
+              onChanged: (value){
+                print('value ${value}');
+              },
+              validator: (value) {
+                if(value!.length < 3){
+                  return 'Mínimo 3 caracteres';
+                }
+              },
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              decoration: InputDecoration(
+                hintText: 'Nombre y apellido',
+                labelText: 'Nombre',
+                helperText: 'Solo letras',
+                //prefix: Icon(Icons.assignment_ind_outlined),
+                // Icon te desplaza a la derecha las cosas para que el icono
+                // no se muestre encima
+                icon: Icon(Icons.assignment_ind_outlined),
+                suffixIcon: Icon(Icons.person_2_outlined),
+                border: OutlineInputBorder(
+                  //borderRadius: BorderRadius.circular(10),
+                  // El zero lo pone rectangular 
+                  //borderRadius: BorderRadius.zero 
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(10),
+                    topRight: Radius.circular(10)
+                  )
+                )
+              ),
+            )
+        ],),
       ),
     );
   }
